12afe7ef7b81e15e10966724a3aaec19e4b89caa
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-10-10 16:42:48 +0700

    Add files via upload

---
+++ DoWhileCuti23_1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DoWhileCuti23_1 {
+    public static void main(String[] args) {
+        Scanner sc23 = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti : ");
+        jatahCuti = sc23.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc23.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari : ");
+                jumlahHari = sc23.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti : " + jatahCuti);
+                } else if (jatahCuti == 0){
+                    break;
+                }else {
+                    System.out.println("Jatah cuti tidak tersedia, sisa jatah cuti anda tinggal " + jatahCuti);
+                }
+            } else {
+                break;
+            }
+        } while (jatahCuti > 0);
+    }
+}
