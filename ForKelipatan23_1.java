33f3040fd1e934216c283ba61f428aa95269b375
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-10-10 13:44:56 +0700

    Add files via upload

---
+++ ForKelipatan23_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ForKelipatan23_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int kelipatan;
+        int jumlah  = 0;
+        int counter = 0;
+        double rataRata = 0.0;
+
+        System.out.print("Masukkan bilangan kelipatan (1 - 9) : ");
+        kelipatan =  scan.nextInt();
+
+        int i = 1;
+        while (i <= 50) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter ++;
+            } 
+            i ++;
+            rataRata = (double)
+            jumlah / counter;
+         }
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata dari kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
+    }
+}
