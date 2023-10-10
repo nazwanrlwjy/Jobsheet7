e919dbe2ca014fc0b973d84e3c6c57809adf85cd
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-10-10 22:03:12 +0700

    Add files via upload

---
+++ While23.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class While23 {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int i =1;
+
+        while (i <= 10){
+            if (i % 2 == 0){
+                System.out.println("Angka " + i + " adalah bilangan genap");
+            }else{
+                System.out.println("Angka " + i +" adalah bilangan ganjil");
+            }
+            i++;
+        }
+    }
+}
