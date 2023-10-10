49c8a8b0eadedc8b0b257df3eeae76450dcc9211
nazwanrlwjy <144089599+nazwanrlwjy@users.noreply.github.com>
2023-10-10 17:09:23 +0700

    Add files via upload

---
+++ For23.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class For23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hasil kuadrat dari " + 1 + " adalah " + (i * i));
+        }
+    }
+}
---
+++ ForVar223.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ForVar223 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int bil;
+        boolean berhenti = false;
+        for (; !berhenti;) {
+            System.out.print("Masukkan bilangan: ");
+            bil = sc.nextInt();
+            System.out.println("Bilangan yang anda masukkan: " + bil);
+            if (bil == 0) {
+                berhenti = true;
+            }
+        }
+         System.out.println("Progam berakhir");
+    }
+}
